667c3732405f157ea00e0d35cc4100ae445e540c
Adam Culp <adamculp@users.noreply.github.com>
2025-02-14 15:17:01 -0500

    refactor

--- main.tf
+++ main.tf
@@ -96,13 +96,13 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
+  listeners = {
+    ex-tcp = {
       port     = 80
       protocol = "HTTP"
       target_group_index = 0
     }
-  ]
+  }
 
   tags = {
     Environment = "Dev"
